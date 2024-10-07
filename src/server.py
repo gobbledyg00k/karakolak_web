a5f9c70b2d2f7691cd24eca9e448d9184afb6146
gobbledyg00k <114208752+gobbledyg00k@users.noreply.github.com>
2024-10-07 21:59:36 +0700

    uncomment rospy

--- src/server.py
+++ src/server.py
@@ -1,17 +1,17 @@
-# import rospy
+import rospy
 
 import os, signal, sys, time
 from flask import Flask, send_from_directory, send_file, request, jsonify, render_template
 
-# rospy.init_node('webserver', anonymous=True)
+rospy.init_node('webserver', anonymous=True)
 
-# port = rospy.get_param('~port', 8080)
-# host = rospy.get_param('~host', '0.0.0.0')
-# www_path = rospy.get_param('~path','../web')
+port = rospy.get_param('~port', 8080)
+host = rospy.get_param('~host', '0.0.0.0')
+www_path = rospy.get_param('~path','../web')
 
 host = '0.0.0.0'
 port = 8080
-# os.system("ustreamer --device=/dev/video0 --host=0.0.0.0 --port=9999 &") #add ./ustreamer to path
+os.system("ustreamer --device=/dev/video0 --host=0.0.0.0 --port=9999 &") #add ./ustreamer to path
 # time.sleep(3)
 
 app = Flask(__name__, static_folder='../web' + '/static', template_folder='../web' + '/templates')
