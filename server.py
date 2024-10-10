1c8cedafb3107069bfa2892715ab8be6ba7366de
gobbledyg00k <114208752+gobbledyg00k@users.noreply.github.com>
2024-10-10 23:19:04 +0700

    add login page

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #import rospy
 
 import os, signal, sys, time
+import hashlib
 from flask import Flask, send_from_directory, send_file, request, jsonify, render_template
 
 #rospy.init_node('webserver', anonymous=True)
@@ -11,8 +12,8 @@ from flask import Flask, send_from_directory, send_file, request, jsonify, rende
 
 host = '0.0.0.0'
 port = 8080
-os.system("ustreamer --device=/dev/video0 --desired-fps=15 --host=0.0.0.0 --port=9998 &")
-os.system("ustreamer --device=/dev/video2 --desired-fps=30 --host=0.0.0.0 --port=9999 &")
+#os.system("ustreamer --device=/dev/video0 --desired-fps=15 --host=0.0.0.0 --port=9998 &")
+#os.system("ustreamer --device=/dev/video2 --desired-fps=30 --host=0.0.0.0 --port=9999 &")
 # time.sleep(3)
 
 app = Flask(__name__, static_folder='web' + '/static', template_folder='web' + '/templates')
@@ -25,14 +26,30 @@ def on_exit(sig, func=None):
     print ("Exit handler triggered")
     sys.exit(1)  
 
+set_exit_handler(on_exit)
+
+database = {'karakolak': '66afc285a26345d250453921bbc86b65dead43c07f3a65a70c4c30ae5050dd5d'}
+
 @app.route('/')
 def server_index():
-    ip_address = request.host.split(':')[0]
-    return render_template('index.html',
-            ros_host = ip_address,
-            # ros_robot = os.uname()[1]
+    return render_template("login.html")
+ 
+@app.route('/control', methods=['POST', 'GET'])
+def login():
+    name = request.form['username']
+    pwd = request.form['password']
+    hash_object = hashlib.sha256(pwd.encode())
+    pwd_hex = hash_object.hexdigest()
+    if name not in database:
+        return render_template('login.html', info = 'Invalid username')
+    else:
+        if database[name] != pwd_hex:
+            return render_template('login.html', info = 'Invalid password')
+        else:
+            ip_address = request.host.split(':')[0]
+            return render_template('index.html',
+                ros_host = ip_address,
+                # ros_robot = os.uname()[1]
             )
-
-set_exit_handler(on_exit)
 
 app.run(host=host, port=port, threaded=True)
